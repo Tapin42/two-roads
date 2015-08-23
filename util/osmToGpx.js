6746c720187f24844f1f39fb7400f04c200dda7f
Joe Navratil <navratil@gmail.com>
2015-08-22 22:00:28 -0700

    Updated to split the OSM data at each intersection

--- util/osmToGpx.js
+++ util/osmToGpx.js
@@ -1,5 +1,6 @@
 var fs      = require('fs');
 var xml2js  = require('xml2js');
+var extend  = require('extend');
 var osmUtil = require('./osmUtil.js');
 
 var inFile = process.argv[2] || (__dirname + '/henry_coe_highways.osm');
@@ -22,8 +23,105 @@ function processData(err, data) {
   var ways = osmUtil.parseHighways(data.osm.way, nodes);
   console.log('# of trails: ' + Object.keys(ways).length);
 
+  splitWays(nodes, ways);
+
   Object.keys(ways).forEach(function (wayId) {
       processSingleWay(ways[wayId])
+    });
+}
+
+function splitWays(nodes, ways) {
+  // 1) Create a dict indexed by node ID
+  // 2) Loop through the ways and push way ID onto the dict for the associated node ID
+  // 3) Filter the node dict to those on more than one way
+  // 4) Foreach node ID
+  //    4a) Foreach way ID
+  //        4aa) See if the way has that node as its first or last point.  If not:
+  //            4aaa) Add the (way, node) to the list of splits
+  // 5) Remap the (way, node) list into { way: [node, node, node] } so we only ever have to hit a way once
+  // 6) Foreach way in the split dict
+  //    6a) Find an appropriate number of new IDs
+  //    6b) Create a new way for each split area with an identical copy of the metadata and just a different nodes list
+
+  var nodesAndTheirWays = {};
+  Object.keys(ways).forEach(function (wayId) {
+      var way = ways[wayId];
+      way.nodes.forEach(function (node) {
+          if (nodesAndTheirWays[node.id]) {
+            nodesAndTheirWays[node.id].push(way.id);
+          } else {
+            nodesAndTheirWays[node.id] = [way.id];
+          }
+        });
+    });
+
+  var intersections = Object.keys(nodesAndTheirWays).filter(function (nodeId) {
+      return nodesAndTheirWays[nodeId].length > 1;
+    });
+
+  console.log('I found ' + intersections.length + ' intersections');
+
+  var splitPoints = [];
+  intersections.forEach(function (nodeId) {
+      nodesAndTheirWays[nodeId].forEach(function (wayId) {
+          var way = ways[wayId];
+          if (way.nodes[0].id !== nodeId && way.nodes[way.nodes.length-1].id !== nodeId) {
+            splitPoints.push({ way: wayId, node: nodeId });
+          }
+        });
+    });
+
+  console.log('I found ' + splitPoints.length + ' split points');
+
+  var splitPointsByWay = {};
+  splitPoints.forEach(function (pt) {
+      if (splitPointsByWay[pt.way]) {
+        splitPointsByWay[pt.way].push(pt.node);
+      } else {
+        splitPointsByWay[pt.way] = [pt.node];
+      }
+    });
+
+  console.log('Looks like ' + Object.keys(splitPointsByWay).length + ' ways are going to be split up.');
+
+  var nextAvailableId = -89999;
+  Object.keys(splitPointsByWay).forEach(function (wayId) {
+      var oldWay = ways[wayId];
+      var splitNodes = splitPointsByWay[wayId];
+      var newWays = {};
+      var curNodes = [];
+
+      var newWayTemplate = {};
+      Object.keys(oldWay).forEach(function (k) {
+        if (k !== 'nodes' && k !== 'id') {
+          newWayTemplate[k] = oldWay[k];
+        }
+      })
+
+      oldWay.nodes.forEach(function (node) {
+          if (splitNodes.length > 0) {
+            curNodes.push(node);
+            if (splitNodes.indexOf(node.id) > -1) {
+              // console.log('We found a split point, and it is ' + curNodes.length + ' nodes into ' + oldWay.name);
+
+              // Cheesy deep copy
+              var newWay = JSON.parse(JSON.stringify(newWayTemplate));
+              extend(newWay, {
+                id: nextAvailableId,
+                nodes: curNodes.slice(0)
+              });
+              nextAvailableId += 1;
+              curNodes = [node];
+              newWays[newWay.id] = newWay;
+
+              splitNodes.splice(splitNodes.indexOf(node.id), 1);
+              // console.log('There are now ' + splitNodes.length + ' more split points for ' + oldWay.name);
+            }
+          }
+        });
+
+      delete ways[wayId];
+      extend(ways, newWays);
     });
 }
 
