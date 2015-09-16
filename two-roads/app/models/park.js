c28bfb81940639a203c0dd8f71559463e61bb496
Joe Navratil <navratil@gmail.com>
2015-09-15 18:24:58 -0700

    Added the rest of the keys for individual parks

--- two-roads/app/models/park.js
+++ two-roads/app/models/park.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string'),
-  dir: DS.attr('string')
+  name: DS.attr(),
+  dir: DS.attr(),
+  start: DS.attr(),
+  trailheads: DS.attr()
 });
