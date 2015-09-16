d603c67f983acd5c69b156e3631dcf3252d09af3
Joe Navratil <navratil@gmail.com>
2015-09-15 17:34:36 -0700

    Well, it's making the right call... and not normalizing.

--- two-roads/app/adapters/application.js
+++ two-roads/app/adapters/application.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'http://localhost:8000'
+  host: 'http://localhost:8000',
+  pathForType: function (typ) {
+    if (typ === 'park') {
+      return '/';
+    }
+    return typ;
+  }
 });
