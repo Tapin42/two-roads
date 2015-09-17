714aaa040ced576377a7558e68bed9a25b5be049
Joe Navratil <navratil@gmail.com>
2015-09-16 23:09:00 -0700

    Added proper linking to /plan/:park

--- two-roads/app/router.js
+++ two-roads/app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('plan');
+  this.route('plan', { path: '/plan/:park' });
   this.route('overview');
   this.route('park');
 });
